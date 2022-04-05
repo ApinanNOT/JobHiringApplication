d762f59289429bbd475858901a3db7c61d1c7cd4
ApinanNOT <not.254355@gmail.com>
2022-04-05 19:25:40 +0700

    end test

--- lib/states/mode.dart
+++ lib/states/mode.dart
@@ -17,7 +17,7 @@ class _ModeState extends State<Mode> {
       appBar: AppBar(
         centerTitle: true,
         title: Text(
-          'ประเภทผู้ใช้งานในการทำงานทดสอบ',
+          'ประเภทผู้ใช้งาน',
           style: MyConstant().headbar(),
         ),
         backgroundColor: MyConstant.primary,
